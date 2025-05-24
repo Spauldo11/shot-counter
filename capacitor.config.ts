9e383683c9701aa09e604503cae991912944b633
Spauldo11 <laird.spaulding@gmail.com>
2025-05-24 09:43:18 -0700

    Removed obsolete file

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.shotcounter.app',
-  appName: 'Shot Counter for Net Sports',
-  webDir: 'build'
-};
-
-export default config;
